2888b7a082514823c1f04e2b4d6b9e6437be9161
zahrafeisal <zahrafeisal3@gmail.com>
2024-08-02 23:42:00 +0300

    used keys and .map()

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,18 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectStuff = projects.map(project => {
+    const { name , id , about , technologies } = project
+    return (
+      <ProjectItem key={id} name={name} about={about} technologies={technologies}/>
+    )
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectStuff}</div>
     </div>
   );
 }
